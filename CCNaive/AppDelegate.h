a22764e6282c6b688cf28994a181c02961b933f9
SolorDreams <SolorDreams@TwistedeMacBook-Pro.local>
2017-06-05 17:41:27 +0800

    ff

---
+++ CCNaive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCNaive
+//
+//  Created by SolorDreams on 2017/6/5.
+//  Copyright © 2017年 SolorDreams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCNaive/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CCNaive
+//
+//  Created by SolorDreams on 2017/6/5.
+//  Copyright © 2017年 SolorDreams. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
